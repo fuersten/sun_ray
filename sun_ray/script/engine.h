b47af566103484e63e5aa2daef6ed59aa8d394dc
Lars-Christian Fürstenberg <lcf@miztli.de>
2021-04-25 21:15:52 +0200

    Added cylinder script object

--- sun_ray/script/engine.h
+++ sun_ray/script/engine.h
@@ -41,6 +41,7 @@
 #include <sun_ray/script/objects/checker_pattern.h>
 #include <sun_ray/script/objects/color.h>
 #include <sun_ray/script/objects/cube.h>
+#include <sun_ray/script/objects/cylinder.h>
 #include <sun_ray/script/objects/disk.h>
 #include <sun_ray/script/objects/gradient_pattern.h>
 #include <sun_ray/script/objects/light.h>
@@ -78,6 +79,7 @@ namespace sunray
         meta_class_registry_.add_meta_class(std::make_shared<CheckerPatternMetaClass>());
         meta_class_registry_.add_meta_class(std::make_shared<ColorMetaClass>());
         meta_class_registry_.add_meta_class(std::make_shared<CubeMetaClass>());
+        meta_class_registry_.add_meta_class(std::make_shared<CylinderMetaClass>());
         meta_class_registry_.add_meta_class(std::make_shared<DiskMetaClass>());
         meta_class_registry_.add_meta_class(std::make_shared<GradientPatternMetaClass>());
         meta_class_registry_.add_meta_class(std::make_shared<LightMetaClass>());
